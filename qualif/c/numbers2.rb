87242dea84b93a186689315a5dc5b7fcc00e4726
YAEGASHI Takeshi <t@keshi.org>
2012-04-14 16:42:07 +0900

    number2.rb: Much faster version.

---
+++ qualif/c/numbers2.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+def rotates(a, b, w)
+  ds = a.to_s
+  rr = {}
+  (w-1).times do
+    ds = ds[w-1,1] + ds[0,w-1]
+    r = ds.to_i
+    rr[r] = true if r > a && r <= b
+  end
+  rr.size
+end
+
+gets.to_i.times do |i|
+  i += 1
+  a, b = gets.chomp.split.map(&:to_i)
+  w = b.to_s.length
+  sum = 0
+  if a < b
+    (a...b).each do |j|
+      sum += rotates(j, b, w)
+    end
+  end
+  puts "Case ##{i}: #{sum}"
+end
